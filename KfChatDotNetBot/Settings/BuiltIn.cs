33e49a4a929d1226f64a49e5c78705a0bb3367c7
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-02-16 21:03:06 +0100

    better embd

--- KfChatDotNetBot/Settings/BuiltIn.cs
+++ KfChatDotNetBot/Settings/BuiltIn.cs
@@ -68,14 +68,14 @@ public static class BuiltIn
         var oldConfigPath = "config.json";
         var logger = LogManager.GetCurrentClassLogger();
         await using var db = new ApplicationDbContext();
-        
+
         logger.Info($"Checking {oldConfigPath} exists");
         if (!Path.Exists(oldConfigPath))
         {
             logger.Info($"{oldConfigPath} does not exist. Migration already performed or was never needed");
             return;
         }
-        
+
         logger.Info($"Migrating {oldConfigPath}");
 #pragma warning disable CS0612 // Type or member is obsolete
         var oldConfig = JsonSerializer.Deserialize<ConfigModel>(await File.ReadAllTextAsync(oldConfigPath));
@@ -111,7 +111,7 @@ public static class BuiltIn
 
         logger.Info("File renamed");
     }
-    
+
     private static void SafelyRenameFile(string oldName, string newName)
     {
         var logger = LogManager.GetCurrentClassLogger();
@@ -129,7 +129,7 @@ public static class BuiltIn
 
     private const string BooleanRegex = "true|false";
     private const string WholeNumberRegex = @"\d+";
-    
+
     public static class Keys
     {
         [BuiltInSetting("Pusher endpoint for Kick", SettingValueType.Text,
@@ -165,18 +165,18 @@ public static class BuiltIn
         public static string TwitchBossmanJackId = "Twitch.BossmanJackId";
         [BuiltInSetting("BossmanJack's Twitch channel username", SettingValueType.Text, "imbossmanjack")]
         public static string TwitchBossmanJackUsername = "Twitch.BossmanJackUsername";
-        [BuiltInSetting("Enable to prevent messages from actually being sent to chat.", SettingValueType.Boolean, 
+        [BuiltInSetting("Enable to prevent messages from actually being sent to chat.", SettingValueType.Boolean,
             "false", BooleanRegex)]
         public static string KiwiFarmsSuppressChatMessages = "KiwiFarms.SuppressChatMessages";
-        [BuiltInSetting("Token to use when authenticating with Discord. Set to null to disable.", 
+        [BuiltInSetting("Token to use when authenticating with Discord. Set to null to disable.",
             SettingValueType.Text, isSecret: true)]
         public static string DiscordToken = "Discord.Token";
         [BuiltInSetting("BossmanJack's Discord user ID", SettingValueType.Text, "554123642246529046", WholeNumberRegex)]
         public static string DiscordBmjId = "Discord.BmjId";
-        [BuiltInSetting("URL for the 16px Twitch icon", SettingValueType.Text, 
+        [BuiltInSetting("URL for the 16px Twitch icon", SettingValueType.Text,
             "https://i.postimg.cc/QMFVV2Xk/twitch16.png")]
         public static string TwitchIcon = "Twitch.Icon";
-        [BuiltInSetting("URL for the 16px Discord icon", SettingValueType.Text, 
+        [BuiltInSetting("URL for the 16px Discord icon", SettingValueType.Text,
             "https://i.postimg.cc/cLmQrp89/discord16.png")]
         public static string DiscordIcon = "Discord.Icon";
         [BuiltInSetting("Bossman's Shuffle Username", SettingValueType.Text, "TheBossmanJack")]
@@ -191,31 +191,31 @@ public static class BuiltIn
         public static string HowlggDivisionAmount = "Howlgg.DivisionAmount";
         [BuiltInSetting("BMJ's user ID on howl.gg", SettingValueType.Text, "951905", WholeNumberRegex)]
         public static string HowlggBmjUserId = "Howlgg.BmjUserId";
-        [BuiltInSetting("Green color used for showing positive values in chat", SettingValueType.Text, 
+        [BuiltInSetting("Green color used for showing positive values in chat", SettingValueType.Text,
             "#3dd179")]
         public static string KiwiFarmsGreenColor = "KiwiFarms.GreenColor";
-        [BuiltInSetting("Red color used for showing negative values in chat", SettingValueType.Text, 
+        [BuiltInSetting("Red color used for showing negative values in chat", SettingValueType.Text,
             "#f1323e")]
         public static string KiwiFarmsRedColor = "KiwiFarms.RedColor";
-        [BuiltInSetting("Bossman's usernames on Jackpot", SettingValueType.Array, 
+        [BuiltInSetting("Bossman's usernames on Jackpot", SettingValueType.Array,
             "[\"TheBossmanJack\", \"Austingambless757\"]")]
         public static string JackpotBmjUsernames = "Jackpot.BmjUsernames";
         [BuiltInSetting("Whether Jackpot is enabled", SettingValueType.Boolean, "true", BooleanRegex)]
         public static string JackpotEnabled = "Jackpot.Enabled";
-        [BuiltInSetting("Bossman's rainbet public IDs", SettingValueType.Array, 
+        [BuiltInSetting("Bossman's rainbet public IDs", SettingValueType.Array,
             "[\"Ir04170wLulcjtePCL7P6lmeOlepRaNp\", \"IA9RHFR1NLHL33AVOM9GL2G2CINM9I6P\"]")]
         public static string RainbetBmjPublicIds = "Rainbet.BmjPublicIds";
-        [BuiltInSetting("URL for your FlareSolverr service API", SettingValueType.Text, 
+        [BuiltInSetting("URL for your FlareSolverr service API", SettingValueType.Text,
             "http://localhost:8191/")]
         public static string FlareSolverrApiUrl = "FlareSolverr.ApiUrl";
         [BuiltInSetting("Proxy in use specifically for FlareSolverr", SettingValueType.Text)]
         public static string FlareSolverrProxy = "FlareSolverr.Proxy";
-        [BuiltInSetting("Bossman's Chips.gg IDs", SettingValueType.Array, 
+        [BuiltInSetting("Bossman's Chips.gg IDs", SettingValueType.Array,
             "[\"1af247cd-67e0-4029-8a93-b8d19c275072\", \"e97ebf3e-d5a8-4583-ab35-5095a05f282e\"]")]
         public static string ChipsggBmjUserIds = "Chipsgg.BmjUserIds";
         [BuiltInSetting("URL for the restream", SettingValueType.Text, "No URL set")]
         public static string RestreamUrl = "RestreamUrl";
-        [BuiltInSetting("Kiwi Farms cookies in key-value pair format", SettingValueType.Complex, 
+        [BuiltInSetting("Kiwi Farms cookies in key-value pair format", SettingValueType.Complex,
             "{}", isSecret: true)]
         public static string KiwiFarmsCookies = "KiwiFarms.Cookies";
         [BuiltInSetting("Length of time the client can go without receiving ANY packets from Sneedchat before " +
@@ -225,7 +225,7 @@ public static class BuiltIn
                         "Note this affects how often the bot will check inactivity of the connection", SettingValueType.Text,
             "10", WholeNumberRegex)]
         public static string KiwiFarmsPingInterval = "KiwiFarms.PingInterval";
-        [BuiltInSetting("FuckUpMode. 0 = Min, 1 = Normal, 2 = Max", SettingValueType.Text, 
+        [BuiltInSetting("FuckUpMode. 0 = Min, 1 = Normal, 2 = Max", SettingValueType.Text,
             "1", WholeNumberRegex)]
         public static string CrackedZalgoFuckUpMode = "Cracked.ZalgoFuckUpMode";
         [BuiltInSetting("FuckUpPosition: 1 = Up, 2 = Middle, 3 = UpAndMiddle, 4 = Bot (Bottom), 5 = UpAndBot, " +
@@ -244,10 +244,10 @@ public static class BuiltIn
         [BuiltInSetting("ISO8601 date of Bossman's rehab end", SettingValueType.Text,
             "2024-10-24T09:00:00-04:00")]
         public static string BotRehabEndTime = "Bot.Rehab.EndTime";
-        [BuiltInSetting("ISO8601 date of Bossman's next PO visit", SettingValueType.Text, 
+        [BuiltInSetting("ISO8601 date of Bossman's next PO visit", SettingValueType.Text,
             "ISO8601 date of Bossman's next PO visit")]
         public static string BotPoNextVisit = "Bot.Po.NextVisit";
-        [BuiltInSetting("ISO8601 date of when Bossman's incarceration began", SettingValueType.Text, 
+        [BuiltInSetting("ISO8601 date of when Bossman's incarceration began", SettingValueType.Text,
             "2024-10-27T03:25:00-05:00")]
         public static string BotJailStartTime = "Bot.Jail.StartTime";
         [BuiltInSetting("JSON array containing court hearings", SettingValueType.Complex, "[]", cacheDurationSeconds: 0)]
@@ -266,19 +266,19 @@ public static class BuiltIn
                         "Fractions will be rounded, so a value of 5 with 7 images will round down and take the oldest image.",
             SettingValueType.Text, "5", WholeNumberRegex)]
         public static string BotImageRandomSliceDivideBy = "Bot.Image.RandomSliceDivideBy";
-        [BuiltInSetting("What the initial value of the Discord GambaSesh temporary bypass variable should be", 
+        [BuiltInSetting("What the initial value of the Discord GambaSesh temporary bypass variable should be",
             SettingValueType.Boolean, "false", BooleanRegex)]
         public static string DiscordTemporarilyBypassGambaSeshInitialValue =
             "Discord.TemporarilyBypassGambaSeshInitialValue";
         // I miss you. Please come back.
-        [BuiltInSetting("Track if Kees has been seen so users can receive a one-time notice if he suddenly shows up", 
+        [BuiltInSetting("Track if Kees has been seen so users can receive a one-time notice if he suddenly shows up",
             SettingValueType.Boolean, "false", BooleanRegex)]
         public static string BotKeesSeen = "Bot.KeesSeen";
         [BuiltInSetting("Whether the Clash.gg integration should be enabled", SettingValueType.Boolean, "true", BooleanRegex)]
         public static string ClashggEnabled = "Clashgg.Enabled";
         [BuiltInSetting("List of IDs that BossmanJack is using on Clash.gg", SettingValueType.Array, "[]")]
         public static string ClashggBmjIds = "Clashgg.BmjIds";
-        [BuiltInSetting("Text to send when reminding people of the Almanac", SettingValueType.Text, 
+        [BuiltInSetting("Text to send when reminding people of the Almanac", SettingValueType.Text,
             "Placeholder text for the Almanac")]
         public static string BotAlmanacText = "Bot.Almanac.Text";
         [BuiltInSetting("Interval for Almanac reminders in seconds", SettingValueType.Text, "14400", WholeNumberRegex)]
@@ -297,7 +297,7 @@ public static class BuiltIn
         [BuiltInSetting("Value in milliseconds for how long the bot should wait before self destructing the inverted pig cube",
             SettingValueType.Text, "5000", WholeNumberRegex)]
         public static string BotImageInvertedPigCubeSelfDestructDelay = "Bot.Image.InvertedPigCubeSelfDestructDelay";
-        [BuiltInSetting("Whether to enable the BetBolt bet feed tracking", SettingValueType.Boolean, "true",  BooleanRegex)]
+        [BuiltInSetting("Whether to enable the BetBolt bet feed tracking", SettingValueType.Boolean, "true", BooleanRegex)]
         public static string BetBoltEnabled = "BetBolt.Enabled";
         [BuiltInSetting("Austin's usernames on BetBolt", SettingValueType.Array, "[\"AustinGambles\"]")]
         public static string BetBoltBmjUsernames = "BetBolt.BmjUsernames";
@@ -309,14 +309,16 @@ public static class BuiltIn
         public static string YeetProxy = "Yeet.Proxy";
         [BuiltInSetting("Cooldown in seconds for the mom command, 0 to disable", SettingValueType.Text, "30", WholeNumberRegex)]
         public static string MomCooldown = "Mom.Cooldown";
-        [BuiltInSetting("Output format to pass to yt-dlp using -o", SettingValueType.Text, 
+        [BuiltInSetting("Output format to pass to yt-dlp using -o", SettingValueType.Text,
             "%(title)s - %(uploader)s [%(id)s] %(upload_date)s %(timestamp)s.mp4")]
         public static string CaptureYtDlpOutputFormat = "Capture.YtDlp.OutputFormat";
         [BuiltInSetting("Working directory set when running yt-dlp", SettingValueType.Text, "/tmp/discord/")]
         public static string CaptureYtDlpWorkingDirectory = "Capture.YtDlp.WorkingDirectory";
         [BuiltInSetting("Path of the yt-dlp binary", SettingValueType.Text, "/usr/local/bin/yt-dlp")]
         public static string CaptureYtDlpBinaryPath = "Capture.YtDlp.BinaryPath";
-        [BuiltInSetting("User-Agent that gets passed to yt-dlp --user-agent", SettingValueType.Text, 
+        [BuiltInSetting("Path of the FFmpeg binary", SettingValueType.Text, "ffmpeg")]
+        public static string FFmpegBinaryPath = "FFmpeg.BinaryPath";
+        [BuiltInSetting("User-Agent that gets passed to yt-dlp --user-agent", SettingValueType.Text,
             "Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0")]
         public static string CaptureYtDlpUserAgent = "Capture.YtDlp.UserAgent";
         [BuiltInSetting("What browser to pass to yt-dlp using --cookies-from-browser", SettingValueType.Text, "firefox")]
@@ -325,7 +327,7 @@ public static class BuiltIn
         public static string CaptureEnabled = "Capture.Enabled";
         [BuiltInSetting("Parent terminal to launch the capture inside of, e.g. xfce4-terminal, mate-terminal, etc. " +
                         "Terminal must support -x. The process detaches from the bot so the bot can be freely restarted while a capture is running. " +
-                        "Not supported on Windows, the bot will use cmd /C + START on Windows.", 
+                        "Not supported on Windows, the bot will use cmd /C + START on Windows.",
             SettingValueType.Text, "/usr/bin/mate-terminal")]
         public static string CaptureYtDlpParentTerminal = "Capture.YtDlp.ParentTerminal";
         [BuiltInSetting("Path to store the temporary .sh script used to initiate the capture", SettingValueType.Text, "/tmp/")]
@@ -335,17 +337,17 @@ public static class BuiltIn
         public static string PartiEnabled = "Parti.Enabled";
         [BuiltInSetting("Path of the streamlink binary", SettingValueType.Text, "/usr/local/bin/streamlink")]
         public static string CaptureStreamlinkBinaryPath = "Capture.Streamlink.BinaryPath";
-        [BuiltInSetting("Output format to pass to streamlink using --output", SettingValueType.Text, 
+        [BuiltInSetting("Output format to pass to streamlink using --output", SettingValueType.Text,
             "{author}-{id}-{title}-{time:%Y-%m-%d_%Hh%Mm%Ss}.ts")]
         public static string CaptureStreamlinkOutputFormat = "Capture.Streamlink.OutputFormat";
-        [BuiltInSetting("Path of the remux script used to convert .ts to .mp4", SettingValueType.Text, 
+        [BuiltInSetting("Path of the remux script used to convert .ts to .mp4", SettingValueType.Text,
             "pwsh /root/BMJ/Convert-TsToMp4.ps1")]
         public static string CaptureStreamlinkRemuxScript = "Capture.Streamlink.RemuxScript";
-        [BuiltInSetting("Special options for Twitch streams captured with Streamlink", SettingValueType.Text, 
+        [BuiltInSetting("Special options for Twitch streams captured with Streamlink", SettingValueType.Text,
             "--twitch-disable-ad --twitch-proxy-playlist=https://eu.luminous.dev," +
             "https://eu2.luminous.dev,https://as.luminous.dev,https://cdn.perfprod.com")]
         public static string CaptureStreamlinkTwitchOptions = "Capture.Streamlink.TwitchOptions";
-        [BuiltInSetting("How often (in seconds) to check if a DLive streamer is live", 
+        [BuiltInSetting("How often (in seconds) to check if a DLive streamer is live",
             SettingValueType.Text, "15", WholeNumberRegex)]
         public static string DLiveCheckInterval = "DLive.CheckInterval";
         // Setting was originally Complex but this was a mistake, it's just an array of usernames. Ditto for PeerTube
@@ -359,7 +361,7 @@ public static class BuiltIn
         public static string KiwiPeerTubeEnabled = "KiwiPeerTube.Enabled";
         [BuiltInSetting("Interval (in seconds) to check live streams", SettingValueType.Text, "10", WholeNumberRegex)]
         public static string KiwiPeerTubeCheckInterval = "KiwiPeerTube.CheckInterval";
-        [BuiltInSetting("Whether to enforce the use of a whitelist (i.e. streamer must be in teh database)", 
+        [BuiltInSetting("Whether to enforce the use of a whitelist (i.e. streamer must be in teh database)",
             SettingValueType.Boolean, "false", BooleanRegex)]
         public static string KiwiPeerTubeEnforceWhitelist = "KiwiPeerTube.EnforceWhitelist";
         [BuiltInSetting("Interval in seconds for checking if the bot is completely dead", SettingValueType.Text, "15", WholeNumberRegex)]
@@ -368,10 +370,10 @@ public static class BuiltIn
         public static string BotExitOnDeath = "Bot.ExitOnDeath";
         [BuiltInSetting("Whether to respond to Bossman impersonation", SettingValueType.Boolean, "true", BooleanRegex)]
         public static string BotRespondToDiscordImpersonation = "Bot.RespondToDiscordImpersonation";
-        [BuiltInSetting("What is the symbol of the bot's currency when used as a suffix for an amount", 
+        [BuiltInSetting("What is the symbol of the bot's currency when used as a suffix for an amount",
             SettingValueType.Text, "KKK")]
         public static string MoneySymbolSuffix = "Money.SymbolSuffix";
-        [BuiltInSetting("What is the symbol of the bot's currency when used as a prefix for an amount", 
+        [BuiltInSetting("What is the symbol of the bot's currency when used as a prefix for an amount",
             SettingValueType.Text, "KKK$")]
         public static string MoneySymbolPrefix = "Money.SymbolPrefix";
         [BuiltInSetting("Whether the monetary system is enabled at all", SettingValueType.Boolean, "false", BooleanRegex)]
@@ -510,6 +512,8 @@ public static class BuiltIn
         public static string KasinoRouletteCountdownDuration = "Kasino.Roulette.CountdownDuration";
         [BuiltInSetting("Whether Xeet posting is enabled", SettingValueType.Boolean, "true", BooleanRegex)]
         public static string XeetEnabled = "Xeet.Enabled";
+        [BuiltInSetting("Maximum video duration in seconds for Xeet embeds", SettingValueType.Text, "120", WholeNumberRegex)]
+        public static string XeetMaxVideoDurationSeconds = "Xeet.MaxVideoDurationSeconds";
         [BuiltInSetting("Connection string for bot's Redis", SettingValueType.Text)]
         public static string BotRedisConnectionString = "Bot.RedisConnectionString";
         [BuiltInSetting("Whether to automatically rehost images when they're added", SettingValueType.Boolean, "true",
