ab3d519f5cb6e697edd336114a144dc53d7c90d2
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-15 09:37:21 +0100

    Add time command

--- KfChatDotNetBot/Commands/TimeCommand.cs
+++ KfChatDotNetBot/Commands/TimeCommand.cs
@@ -8,14 +8,32 @@ namespace KfChatDotNetBot.Commands;
 public class TimeCommand : ICommand
 {
     public List<Regex> Patterns => [new Regex("^time")];
-    public string? HelpText => "Get current time in BMT";
+    public string? HelpText => "Get current time in FTT";
     public UserRight RequiredRight => UserRight.Guest;
     public TimeSpan Timeout => TimeSpan.FromSeconds(10);
     public RateLimitOptionsModel? RateLimitOptions => null;
     public async Task RunCommand(ChatBot botInstance, MessageModel message, UserDbModel user, GroupCollection arguments, CancellationToken ctx)
     {
-        var bmt = new DateTimeOffset(TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow,
-            TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")), TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").BaseUtcOffset);
-        await botInstance.SendChatMessageAsync($"It's currently {bmt:dddd h:mm:ss tt} FTT");
+        var estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+        var nowEst = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, estZone);
+        var ftt = new DateTimeOffset(nowEst, estZone.BaseUtcOffset);
+
+        var targetEst = new DateTime(2026, 3, 15, 16, 0, 0, DateTimeKind.Unspecified);
+        var targetOffset = new DateTimeOffset(targetEst, estZone.BaseUtcOffset);
+
+        string extraInfo;
+        if (ftt < targetOffset)
+        {
+            var remaining = targetOffset - ftt;
+            extraInfo = $" | {(int)remaining.TotalDays}d {remaining.Hours}h {remaining.Minutes}m {remaining.Seconds}s until Fishtank starts";
+        }
+        else
+        {
+            var elapsed = ftt - targetOffset;
+            var days = (int)elapsed.TotalDays;
+            extraInfo = $" on day {days}";
+        }
+
+        await botInstance.SendChatMessageAsync($"It's currently {ftt:dddd h:mm:ss tt} FTT{extraInfo}");
     }
 }
