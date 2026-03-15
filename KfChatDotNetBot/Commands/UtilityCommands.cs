77544fffd7f9dd4bbd305676d942627e65d80a5e
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-15 20:24:53 +0100

    Add multiview

--- KfChatDotNetBot/Commands/UtilityCommands.cs
+++ KfChatDotNetBot/Commands/UtilityCommands.cs
@@ -146,6 +146,6 @@ public class PPVCommand : ICommand
         CancellationToken ctx)
     {
         var url = await SettingsProvider.GetValueAsync(BuiltIn.Keys.RestreamUrl);
-        await botInstance.SendChatMessageAsync($"@{message.Author.Username}, restream URL: https://old.ppv.to/ft, MPV playlist: https://i.ddos.lgbt/u/fishtank-s5.m3u", true);
+        await botInstance.SendChatMessageAsync($"@{message.Author.Username}, restream URL: https://old.ppv.to/ft, MPV playlist: https://i.ddos.lgbt/u/fishtank-s5.m3u, Camera multiview: https://kiwifarms.st/posts/23969413", true);
     }
 }
