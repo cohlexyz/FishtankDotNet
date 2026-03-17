54dc72a422e90bc696799be749e656012e38bd2f
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-17 22:08:35 +0100

    update bot help

--- KfChatDotNetBot/Commands/UtilityCommands.cs
+++ KfChatDotNetBot/Commands/UtilityCommands.cs
@@ -28,7 +28,7 @@ public class HelpCommand : ICommand
     };
     public async Task RunCommand(ChatBot botInstance, MessageModel message, UserDbModel user, GroupCollection arguments, CancellationToken ctx)
     {
-        await botInstance.SendChatMessageAsync("Bot usage guide: https://kiwifarms.st/posts/23976561", true);
+        await botInstance.SendChatMessageAsync("Bot usage guide: https://fishtank.observer/bot/", true);
     }
 }
 
