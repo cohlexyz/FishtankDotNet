b78593ada61252df62b4d5e54c08c8a9357774d3
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-18 18:37:33 +0100

    fuck

--- KfChatDotNetBot/Commands/AdminCommands.cs
+++ KfChatDotNetBot/Commands/AdminCommands.cs
@@ -68,7 +68,7 @@ public class SetMotdCommand : ICommand
             return;
         }
 
-        await botInstance.SendChatMessageAsync($"‎{motd}", true);
+        await botInstance.SendChatMessageAsync($"❗{motd}", true);
     }
 }
 
