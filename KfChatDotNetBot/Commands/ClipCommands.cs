6e164dd442d087db83e1744ed6d1aea5962e3fe2
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-19 14:39:23 +0100

    fix url

--- KfChatDotNetBot/Commands/ClipCommands.cs
+++ KfChatDotNetBot/Commands/ClipCommands.cs
@@ -65,7 +65,7 @@ public static class FishtankCameras
             }
             else if (pendingName != null && line.Length > 0 && !line.StartsWith('#'))
             {
-                cameras[pendingName] = line;
+                cameras[pendingName] = line.Replace("/hls", "");
                 pendingName = null;
             }
         }
