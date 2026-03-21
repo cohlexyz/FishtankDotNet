09f656c243efd087a81cda0378be6a9d53e9c9e4
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-21 16:42:06 +0100

    auto delete

--- KfChatDotNetBot/Commands/Kasino/KasinoUserCommands.cs
+++ KfChatDotNetBot/Commands/Kasino/KasinoUserCommands.cs
@@ -31,7 +31,7 @@ public class GetBalanceCommand : ICommand
     {
         var gambler = await Money.GetGamblerEntityAsync(user.Id, ct: ctx);
         await botInstance.SendChatMessageAsync(
-            $"{user.FormatUsername()}, your balance is {await gambler!.Balance.FormatKasinoCurrencyAsync()}", true, whisperTo: user.KfId);
+            $"{user.FormatUsername()}, your balance is {await gambler!.Balance.FormatKasinoCurrencyAsync()}", true, whisperTo: user.KfId, autoDeleteAfter: TimeSpan.FromSeconds(10));
 
         if (botInstance.BotServices.KasinoShop != null)
         {
