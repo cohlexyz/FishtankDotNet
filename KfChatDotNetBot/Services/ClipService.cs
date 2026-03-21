77a9c7432dce2c9a79f2236f7d2276cdc0542540
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-21 12:28:55 +0100

    Add watermark

--- KfChatDotNetBot/Services/ClipService.cs
+++ KfChatDotNetBot/Services/ClipService.cs
@@ -600,9 +600,12 @@ public class ClipService
     {
         var id = Guid.NewGuid().ToString("N")[..8];
         var mp4Path = Path.Combine(Path.GetTempPath(), $"clip_{cameraName.Replace(' ', '_')}_{id}.mp4");
-
-        var ffmpegArgs = $"-fflags +discardcorrupt -i \"{tsPath}\" -vf \"scale=-2:720,setpts=PTS-STARTPTS\" -af asetpts=PTS-STARTPTS " +
-                         $"-c:v libx264 -preset veryfast -crf 26 -c:a aac -b:a 128k -threads 6 -progress pipe:1 -y \"{mp4Path}\"";
+        var ffmpegArgs = $"-fflags +discardcorrupt -i \"{tsPath}\" -i \"./Assets/watermark.png\" " +
+            $"-filter_complex \"[0:v]scale=-2:720,setpts=PTS-STARTPTS[vid];" +
+            $"[vid][1:v]overlay=10:H-h-10\" " +
+            $"-af asetpts=PTS-STARTPTS " +
+            $"-c:v libx264 -preset veryfast -crf 26 -c:a aac -b:a 128k " +
+            $"-threads 6 -progress pipe:1 -y \"{mp4Path}\"";
 
         var processInfo = new ProcessStartInfo
         {
