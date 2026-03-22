bc75476c723fc61ab930926f026ca3e5068f22b0
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-22 11:32:29 +0100

    whisper clips when chat is active

--- KfChatDotNetBot/Commands/ClipCommands.cs
+++ KfChatDotNetBot/Commands/ClipCommands.cs
@@ -329,7 +329,7 @@ public class ClipSaveCommand : ICommand
             clipService.ClearMarker(markerCameraName);
 
         // Send the clip URL as a new message and delete the progress message
-        await botInstance.SendChatMessageAsync($"@{user.KfUsername}, here's your clip: {result}", true);
+        await botInstance.SendChatMessageAsync($"@{user.KfUsername}, here's your clip: {result}", true, whisperTo: user.KfId);
         if (gotUuid)
             await botInstance.KfClient.DeleteMessageAsync(sent.ChatMessageUuid!);
     }
