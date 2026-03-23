d8861b459c484c70852a076ded77f4359306e7bf
cohlexyz <142505474+cohlexyz@users.noreply.github.com>
2026-03-23 20:45:25 +0100

    always whisper that one

--- KfChatDotNetBot/Commands/ClipCommands.cs
+++ KfChatDotNetBot/Commands/ClipCommands.cs
@@ -331,7 +331,7 @@ public class ClipSaveCommand : ICommand
         // Send the clip URL as a new message and delete the progress message
         // also send to normal chat
         await botInstance.SendChatMessageAsync($"@{user.KfUsername}, here's your clip: {result}", true);
-        await botInstance.SendChatMessageAsync($"@{user.KfUsername}, here's your clip: {result}", true, whisperTo: user.KfId);
+        await botInstance.SendWhisperAsync(user.KfId, $"@{user.KfUsername}, here's your clip: {result}");
         if (gotUuid)
             await botInstance.KfClient.DeleteMessageAsync(sent.ChatMessageUuid!);
     }
